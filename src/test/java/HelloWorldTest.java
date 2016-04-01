2d71f3af80020a4b630f5f79df125708f0ed42c1
Vyacheslav Lapin <Vyacheslav_Lapin@epam.com>
2016-04-01 17:46:16 +0300

    property

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -11,44 +11,45 @@ import static org.junit.Assert.assertEquals;
 
 public class HelloWorldTest {
 
-	protected static final String APPLICATION_CONTEXT_XML_FILE_NAME = "resources/application-context.xml";
+    private static final String APPLICATION_CONTEXT_XML_FILE_NAME =
+            "src/main/resources/application-context.xml";
 
-	private UsualPerson expectedPerson;
+    private Person expectedPerson;
 
-	private AbstractApplicationContext context;
+    private AbstractApplicationContext context;
 
-	@Before
-	public void setUp() throws Exception {
-		context = new FileSystemXmlApplicationContext(
-				new String[] { APPLICATION_CONTEXT_XML_FILE_NAME });
-		expectedPerson = getExpectedPerson();
-	}
+    @Before
+    public void setUp() throws Exception {
+        context = new FileSystemXmlApplicationContext(
+                new String[]{APPLICATION_CONTEXT_XML_FILE_NAME});
+        expectedPerson = getExpectedPerson();
+    }
 
-	@Test
-	public void testInitPerson() {
-		UsualPerson person = (UsualPerson) context.getBean("person", Person.class);
-		assertEquals(expectedPerson, person);
-		System.out.println(person);
-	}
+    @Test
+    public void testInitPerson() {
+        Person person = context.getBean("person", Person.class);
+        assertEquals(expectedPerson, person);
+        System.out.println(person);
+    }
 
-	private UsualPerson getExpectedPerson() {
-		UsualPerson person = new UsualPerson();
-		person.setAge(35);
-		person.setName("John Smith");
+    private UsualPerson getExpectedPerson() {
+        UsualPerson person = new UsualPerson();
+        person.setAge(35);
+        person.setName("John Smith");
 
-		Country country = new Country();
-		country.setId(1);
-		country.setName("Russia");
-		country.setCodeName("RU");
+        Country country = new Country();
+        country.setId(1);
+        country.setName("Russia");
+        country.setCodeName("RU");
 
-		person.setCountry(country);
+        person.setCountry(country);
 
-		return person;
-	}
-	
-	@After
-	public void tearDown() throws Exception{
-		if (context != null)
-			context.close();
-	}
+        return person;
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (context != null)
+            context.close();
+    }
 }
