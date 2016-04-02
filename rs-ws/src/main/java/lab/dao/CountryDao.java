6b4631ea4ffd70277539b8f9378ef2451aee53d2
Vyacheslav Lapin <Vyacheslav_Lapin@epam.com>
2016-04-02 04:46:18 +0300

    rs-ws module files created.

---
+++ rs-ws/src/main/java/lab/dao/CountryDao.java
@@ -0,0 +1,15 @@
+package lab.dao;
+
+import lab.model.Country;
+
+import java.util.List;
+
+public interface CountryDao {
+
+    void save(Country country);
+
+    List<Country> getAllCountries();
+
+    Country getCountryByName(String name);
+
+}
