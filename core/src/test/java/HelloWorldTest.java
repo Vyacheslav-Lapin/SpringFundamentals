fc46f15765f62131432fd250edba3d34e1ee36f4
Vyacheslav Lapin <Vyacheslav_Lapin@epam.com>
2016-04-06 16:22:20 +0300

    MVC-модулю проставлены стартовые файлы

--- core/src/test/java/HelloWorldTest.java
+++ core/src/test/java/HelloWorldTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.After;
-import static com.sun.xml.internal.ws.dump.LoggingDumpTube.Position.Before;
 import static org.junit.Assert.assertEquals;
 
 public class HelloWorldTest {
---
+++ mvc/src/main/java/lab/db/hsql/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE user (
+  id IDENTITY,
+  firstname VARCHAR(20),
+  lastname  VARCHAR(30)
+)
